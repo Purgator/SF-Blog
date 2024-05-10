41515c2fdcfa9e420349622927e78d499974e346
Aymeric <ari@softfluent. com>
2024-05-10 18:22:10 +0200

    ✨ Add 'TimeProvider' project and its first test SystemClockTests:
    The 'TimeProvider' project and its corresponding tests have been added to the solution. The tests include the implementation of a fake clock to generate a specific date-time value. Necessary dependencies like NUnit, FluentAssertions, and Microsoft.Extensions.TimeProvider.Testing are included.

---
+++ TimeProvider/SF.Blog.TimeProvider.Tests/SystemClockTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+
+namespace Blog;
+
+public sealed class SystemClockTests
+{
+    [Test]
+    public void a_stock_date_cannot_be_tested_out()
+    {
+        ISystemClock clock = new SystemClock();
+        var document = $"I have no idea what time it is {clock.UtcNow}";
+    }
+
+    [Test]
+    public void the_fake_implementation_can_be_controlled_to_return_specific_value()
+    {
+        ISystemClock clock = new FakeClock();
+        var document = $"I have no idea what time it is {clock.UtcNow:yy/MM/dd}";
+        document.Should().Be( $"I have no idea what time it is 24/05/10" );
+    }
+
+    public sealed class FakeClock : ISystemClock
+    {
+        public DateTime UtcNow => DateTime.Parse( "2024-05-10" );
+    }
+}
+
+public interface ISystemClock
+{
+    DateTime UtcNow { get; }
+}
+
+public sealed class SystemClock : ISystemClock
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
